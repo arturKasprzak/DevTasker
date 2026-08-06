6ce2cd21dd99729076c89beb121cf18dce3aefa1
Artur Kasprzak <kasprzak@vialutions.com>
2026-08-06 13:39:59 +0200

    Add DevTaskerDbContext

---
+++ src/DevTasker.Infrastructure/DevTaskerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DevTasker.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevTasker.Infrastructure;
+
+public class DevTaskerDbContext : DbContext
+{
+    public DevTaskerDbContext(DbContextOptions<DevTaskerDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<User> Users { get; set; }
+}
